500651eac65b6c1783636a964a5ad2dc12908a32
Thato <t.chitja369@gmail.com>
2023-11-24 13:14:42 +0200

    Modify the Attributes

--- Person/src/Second.java
+++ Person/src/Second.java
@@ -2,6 +2,9 @@ public class Second {
     public static void main(String[] args) throws Exception {
         // Access the attribute
         App Object1 = new App();
+
+        // Modify/Override the Attribute
+        Object1.x = 40;
         System.out.println(Object1.x);       
     }
 }
