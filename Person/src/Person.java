2a335d1e763b60136102f3b2e4279fb8ad3d3c0b
Thato <t.chitja369@gmail.com>
2023-11-24 13:36:47 +0200

    Change App class into Person class

--- Person/src/Person.java
+++ Person/src/Person.java
@@ -1,8 +1,13 @@
-public class App {
+public class Person {
     // Set attributes/fields
     String first_name = "John";
     String last_name = "Doe";
     int age = 24;
+
+    // Set method
+    static void myMethod(){
+        System.out.println("Hello World!");
+    }
 
     // Remove the ability to override the value
     final int identity_number = 56720936;   
--- Person/src/Second.java
+++ Person/src/Second.java
@@ -1,7 +1,7 @@
 public class Second {
     public static void main(String[] args) throws Exception {
         // Access the attribute
-        App Object1 = new App();
+        Person Object1 = new Person();
 
         // Display the Attributes
         System.out.println("Name: " + Object1.first_name + " " + Object1.last_name);
