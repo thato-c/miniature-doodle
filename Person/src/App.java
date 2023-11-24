900954dc1faab91a7f6e4c03e874316bf32a8e6a
Thato <t.chitja369@gmail.com>
2023-11-24 13:05:13 +0200

    Initial Commit

---
+++ Person/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    // Set attributes/fields
+    int x = 5;
+    int y = 3;   
+}
---
+++ Person/src/Second.java
@@ -0,0 +1,7 @@
+public class Second {
+    public static void main(String[] args) throws Exception {
+        // Access the attribute
+        App Object1 = new App();
+        System.out.println(Object1.x);       
+    }
+}
